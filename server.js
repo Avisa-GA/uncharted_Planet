c773f277f9fce76515208caf0dd77bd1bc1dcca8
Avisa-SEI <aap571420@gmail.com>
2021-06-04 12:49:16 -0700

    Add: Delete route

--- server.js
+++ server.js
@@ -4,9 +4,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const methodOverride = require('method-override')
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: false}))
+app.use(methodOverride('_method'))
 
 // DATABASE
 const planetInfos = require('./models/planetInfos.js')
@@ -14,6 +16,13 @@ const planetInfos = require('./models/planetInfos.js')
 // NEW ROUTE TO START
 app.get('/', (req, res) => {
     res.send('this is the new route')
+})
+
+// INDEX
+app.get('/planet', (req, res) => {
+    res.render('index.ejs', {
+        allScientist: planetInfos
+    })
 })
 
 // NEW
@@ -26,6 +35,15 @@ app.post('/planet' ,(req, res) => {
       planetInfos.push(req.body)
       console.log('req.body is: ', req.body);
       res.redirect('/planet')
+})
+
+// SHOW
+
+
+// DELETE
+app.delete('/planet/:index', (req, res) => {
+    planetInfos.splice(req.params.index, 1)
+    res.redirect('/planet')
 })
 
 
